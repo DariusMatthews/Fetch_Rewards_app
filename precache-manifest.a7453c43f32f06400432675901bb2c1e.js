9abdc4d69602abb8d3fe8795de59bc1642f19f41
Darius Matthews <DariustheDeveloper@gmail.com>
2020-09-11 00:36:40 -0500

    Updates

---
+++ precache-manifest.a7453c43f32f06400432675901bb2c1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e7659bf053b4d6d9ade361a2a4bc8d8",
+    "url": "//dariusmatthews.github.io/Fetch_Rewards_app/index.html"
+  },
+  {
+    "revision": "dec8efd4b17fe047b572",
+    "url": "//dariusmatthews.github.io/Fetch_Rewards_app/static/css/main.b94dc928.chunk.css"
+  },
+  {
+    "revision": "50cda2cf769e8a584c88",
+    "url": "//dariusmatthews.github.io/Fetch_Rewards_app/static/js/2.f89e4654.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "//dariusmatthews.github.io/Fetch_Rewards_app/static/js/2.f89e4654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dec8efd4b17fe047b572",
+    "url": "//dariusmatthews.github.io/Fetch_Rewards_app/static/js/main.b41fc199.chunk.js"
+  },
+  {
+    "revision": "08de4566aaee863ba418",
+    "url": "//dariusmatthews.github.io/Fetch_Rewards_app/static/js/runtime-main.5087341d.js"
+  }
+]);
